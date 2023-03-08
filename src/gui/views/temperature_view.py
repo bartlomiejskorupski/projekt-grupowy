cbdb17232e06c0c65ced9ae415b396c46b643842
Bartlomiej Skorupski <bartmann001@gmail.com>
2023-03-08 20:57:19 +0100

    display temperature plot in app

--- src/gui/views/temperature_view.py
+++ src/gui/views/temperature_view.py
@@ -1,9 +1,12 @@
-from guizero import Box, Text, PushButton
+from guizero import Box, Text, PushButton, Picture
 from src.database.local_context import LocalContext
 from env import DEBUG_BORDER
 from src.misc.utils import addPadding
 from src.model.reading import ReadingType
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
 
 import src.misc.logger as logger
 LOG = logger.getLogger(__name__)
@@ -13,6 +16,10 @@ class TemperatureView:
   db_context: LocalContext
   container: Box
   
+  plot_pic: Picture
+  PLOT_WITDH = 360
+  PLOT_HEIGHT = 190
+
   def __init__(self, main_window):
     self.main_window = main_window
     self.db_context = self.main_window.db_context
@@ -34,6 +41,12 @@ class TemperatureView:
       text='Temperature',
       size=10)
     
+    self.plot_pic = Picture(
+      self.container,
+      align='top',
+      width=360,
+      height=190)
+
     debug_button = PushButton(
       self.container,
       align='bottom',
@@ -44,6 +57,23 @@ class TemperatureView:
     date_from = datetime.now().replace(hour=0, minute=0, second=0) - timedelta(days=1)
     date_to = datetime.now().replace(hour=23, minute=59, second=59)
     readings = self.db_context.fetch_readings(ReadingType.TEMPERATURE, date_from, date_to)
-    #readings = self.db_context.fetch_all()
+
+    filtered =  [
+      (datetime.strptime(date_str, self.db_context.DATETIME_FORMAT), value)
+      for (_, date_str, value, type) in readings
+      if type == ReadingType.TEMPERATURE.value
+    ]
+    (dates, values) = list(zip(*filtered))
+
+    plt.figure()
+    plt.plot(dates, values)
+    plt.title('Temperature')
+    plt.axis([dates[0], dates[-1], 0, 30])
+    image_buffer = BytesIO()
+    plt.savefig(image_buffer, format='png')
+    plt.close()
+    image = Image.open(image_buffer)
+    self.plot_pic.image = image
+    image_buffer.close()
 
 
